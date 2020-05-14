fa728cedc009b9b697907871b2044679c044a992
tiwariaditya15 <tiwariaditya15@hotmail.com>
2020-05-14 17:30:22 +0530

    Node

---
+++ app.js
@@ -0,0 +1,24 @@
+// --> video 4
+// /* setTimeout sits on global object and acts like timer */
+// setTimeout(function(){
+//     console.log('2 secconds passed now setInterval runs.');
+// }, 2000);
+
+
+
+// let time = 0;
+// /*setInterval() runs after each x miliseconds passed in function*/
+// const timer = setInterval(function(){
+//     time += 2;
+//     if(time > 5){
+//         console.log('setInterval ran for 4 seconds now exiting.');
+//         clearInterval(timer);
+//     }
+// }, 2000);
+
+// //global.__dirname and ____filename
+// console.log(global);
+// console.log( 'Directory -> ' + __dirname);
+// console.log( 'Filename -> ' + __filename);
+
+//--> video 5
