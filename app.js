cbe9bb058dac2a6ed55aed1ed4fd3b9dc7d201c0
tiwariaditya15 <tiwariaditya15@hotmail.com>
2020-05-18 21:44:47 +0530

    Node

--- app.js
+++ app.js
@@ -1,47 +1,26 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-   if ( req.url === '/home' || req.url === '/'){
-       res.writeHeader(200, {'Content-Type': 'text/html'});
-       fs.createReadStream( __dirname + '/index.html', 'utf8').pipe(res);
-   } else if ( req.url === '/contact'){
-        res.writeHeader(200, {'Content-Type': 'text/html'});
-        fs.createReadStream( __dirname + '/contact.html', 'utf8').pipe(res);
-    } else if ( req.url === '/pricing'){
-        res.writeHeader(200, {'Content-Type': 'text/html'});
-        fs.createReadStream( __dirname + '/pricing.html', 'utf8').pipe(res);
-    }else if( req.url === '/api/candidates'){
-     const details = [{
-        "first": {
-             "name" : "aditya",
-             "age": 18,
-             "experience": "freshers"
-     
-         },
-     
-         "second": {
-             "name" : "pankaj",
-             "age": 29,
-             "experience": "5"
-     
-         },
-     
-         "third": {
-             "name" : "priya",
-             "age": 26,
-             "experience": "none"
-     
-         } 
-     
-     }];
-     res.writeHeader(200, {'Content-Type':'application/json'});
-     res.end(JSON.stringify(details));
-    }else{
-        res.writeHeader(404, {'Content-Type': 'text/html'});
-        fs.createReadStream( __dirname + '/404.html', 'utf8').pipe(res);
-    }
+const app = express();
+
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('Server started.');
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile( __dirname + '/contact.html');
+});
+
+app.get('/pricing', (req, res) => {
+    res.sendFile( __dirname + '/pricing.html');
+});
+
+app.get('/profile/:name/:id', (req, res) => {
+    res.render('profile', {user: req.params.name, id: req.params.id});
+});
+
+app.listen(3000);
---
+++ nodeRouting.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+   if ( req.url === '/home' || req.url === '/'){
+       res.writeHeader(200, {'Content-Type': 'text/html'});
+       fs.createReadStream( __dirname + '/index.html', 'utf8').pipe(res);
+   } else if ( req.url === '/contact'){
+        res.writeHeader(200, {'Content-Type': 'text/html'});
+        fs.createReadStream( __dirname + '/contact.html', 'utf8').pipe(res);
+    } else if ( req.url === '/pricing'){
+        res.writeHeader(200, {'Content-Type': 'text/html'});
+        fs.createReadStream( __dirname + '/pricing.html', 'utf8').pipe(res);
+    }else if( req.url === '/api/candidates'){
+     const details = [{
+        "first": {
+             "name" : "aditya",
+             "age": 18,
+             "experience": "freshers"
+     
+         },
+     
+         "second": {
+             "name" : "pankaj",
+             "age": 29,
+             "experience": "5"
+     
+         },
+     
+         "third": {
+             "name" : "priya",
+             "age": 26,
+             "experience": "none"
+     
+         } 
+     
+     }];
+     res.writeHeader(200, {'Content-Type':'application/json'});
+     res.end(JSON.stringify(details));
+    }else{
+        res.writeHeader(404, {'Content-Type': 'text/html'});
+        fs.createReadStream( __dirname + '/404.html', 'utf8').pipe(res);
+    }
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('Server started.');
