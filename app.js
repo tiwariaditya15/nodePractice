2275edb9c8641e1c8000b8a53b180c42ada4af64
tiwariaditya15 <tiwariaditya15@hotmail.com>
2020-05-20 13:09:47 +0530

    Node

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const urlencodedParser = bodyParser.urlencoded({extended: false});
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 
 app.get('/home', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 
 app.get('/contact', (req, res) => {
-    res.sendFile( __dirname + '/contact.html');
+    res.render('contact');
 });
 
 app.get('/pricing', (req, res) => {
-    res.sendFile( __dirname + '/pricing.html');
+    res.render('pricing', {queryString: req.query});
+});
+
+app.post('/pricing', urlencodedParser, (req, res) => {
+        console.log(req.body);
+        res.render('pricing-fix', {data: req.body});
 });
 
 app.get('/profile/:name/:id', (req, res) => {
-    res.render('profile', {user: req.params.name, id: req.params.id});
+    const data = [];
+    res.render('profile', {user: req.params.name, id: req.params.id, data: data});
 });
 
 app.listen(3000);
